ad5aa674f7128931b9eb629fc4e313299858b166
leopaul29 <leopaulmartin.iut@gmail.com>
2022-06-27 23:51:58 +0200

    2 files modified, 1 deleted

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,17 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StocksManagerService } from './stocks-manager.service';
 import { StocksService } from './stock.service';
-import { FilterStocksService } from './filter-stocks.service';
 import { CustomLoaderService } from './custom-loader.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [
-    StocksManagerService,
-    StocksService,
-    FilterStocksService,
-    CustomLoaderService,
-  ],
+  providers: [StocksManagerService, StocksService, CustomLoaderService],
 })
 export class CoreModule {}
--- src/app/stocks/stock-list/stock-list.component.ts
+++ src/app/stocks/stock-list/stock-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, of, Subscription } from 'rxjs';
-import { FilterStocksService } from '../../core/filter-stocks.service';
+import { Observable, Subscription } from 'rxjs';
 import { StocksManagerService } from '../../core/stocks-manager.service';
 import { IStock } from '../../models/stock.model';
 
@@ -10,20 +9,11 @@ import { IStock } from '../../models/stock.model';
   styleUrls: ['stock-list.component.css'],
 })
 export class StockListComponent implements OnInit, OnDestroy {
-  sortBy: string;
-  visibleStockList: IStock[];
-  visibleStockList$: Observable<IStock[]>;
   stockList: IStock[];
   stockList$: Observable<IStock[]>;
   stockListSubscription: Subscription;
 
-  constructor(
-    private stocksManager: StocksManagerService,
-    private filterStocksService: FilterStocksService
-  ) {
-    this.sortBy = '';
-    this.visibleStockList = [];
-    this.visibleStockList$ = new Observable<IStock[]>();
+  constructor(private stocksManager: StocksManagerService) {
     this.stockList = [];
     this.stockList$ = new Observable<IStock[]>();
     this.stockListSubscription = new Subscription();
@@ -35,16 +25,6 @@ export class StockListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.stockListSubscription.unsubscribe();
-  }
-
-  applyFilter(filter: string): void {
-    if (!filter) return;
-
-    this.sortBy = filter;
-    this.visibleStockList = this.filterStocksService.filterStocks(
-      filter,
-      this.stockList
-    );
   }
 
   clearAll(): void {
